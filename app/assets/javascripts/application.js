c8cab49156d90e0e314e4e12279a17bfea2e3460
Karen Ling <karensling@gmail.com>
2015-07-01 12:09:46 -0700

    Redo logic for displaying portfolio images.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -42,54 +42,54 @@ $(document).ready(function(){
     $('.laptop').css('margin-top', laptopWidth / 1.74 / -2);
 
 
+    //
+    // $(window).scroll(function () {
+    //   var currentPos = $('body').scrollTop();
+    //   if (currentPos < windowHeight * 3) { // IMPORTANT TO SET THIS TO THE NUMBER OF IMAGES THERE ARE
+    //
+    //     if (currentPos >= 0 && currentPos < windowHeight) {
+    //       $('.main').css('position', 'fixed');
+    //       $('.main').css('top', 0);
+    //       $('.portfolio').css('background-image', "url('http://images.karenling.net/aa/portfolio/portfolio_image_1.jpg')");
+    //       $('.portfolio').html("<div class='description'><div class='content'><div class='title'><a href='http://flappypig.karenling.net/'>FlappyPig <i class='fa fa-external-link'></i></a></div><div class='icons'><img src='https://dl.dropboxusercontent.com/u/2330299/capstone/portfolio/code_icons/javascript.png'><img src='https://dl.dropboxusercontent.com/u/2330299/capstone/portfolio/code_icons/html5.png'></div><div class='body'>Highly addictive video game, inspired by Flappy Bird, BUT with a flying pig! I built the game using JavaScript and HTML5 Canvas. &nbsp;<a href='http://github.com/karenling/Flappy-Pig'><i class='fa fa-github'></i></a></div></div></div>");
+    //     } else if (currentPos >= windowHeight && currentPos < windowHeight * 2) {
+    //       $('.main').css('position', 'fixed');
+    //       $('.main').css('top', 0);
+    //       $('.portfolio').css('background-image', "url('http://images.karenling.net/aa/portfolio/portfolio_image_2.jpg')");
+    //       $('.portfolio').html("<div class='description'><div class='content'><div class='title'><a href='http://twinkieset.com'>Twinkieset <i class='fa fa-external-link'></i></a></div><div class='icons'><img src='https://dl.dropboxusercontent.com/u/2330299/capstone/portfolio/code_icons/rails.png'><img src='https://dl.dropboxusercontent.com/u/2330299/capstone/portfolio/code_icons/ruby.png'><img src='https://dl.dropboxusercontent.com/u/2330299/capstone/portfolio/code_icons/javascript.png'><img src='https://dl.dropboxusercontent.com/u/2330299/capstone/portfolio/code_icons/backbonejs.png'><img src='https://dl.dropboxusercontent.com/u/2330299/capstone/portfolio/code_icons/html5.png'><img src='https://dl.dropboxusercontent.com/u/2330299/capstone/portfolio/code_icons/css3.png'></div><div class='body'>Twinkieset is a client photo gallery for modern photographers, inspired by <a href='http://pixieset.com'>Pixieset</a>, built on Rails and Backbone. Photographers can upload photos and create beautifully designed galleries for their clients. &nbsp;<a href='http://github.com/karenling/Twinkieset'><i class='fa fa-github'></i></a></div></div></div>");
+    //     } else if (currentPos >= windowHeight * 2 && currentPos < windowHeight * 3) {
+    //       $('.main').css('position', 'fixed');
+    //       $('.main').css('top', 0);
+    //       $('.portfolio').css('background-image', "url('http://images.karenling.net/aa/portfolio/portfolio_image_3.jpg')");
+    //       $('.portfolio').html("<div class='description'><div class='content'><div class='title'><a href='http://github.com/karenling/chess'>Ruby Chess <i class='fa fa-external-link'></i></a></div><div class='icons'><img src='https://dl.dropboxusercontent.com/u/2330299/capstone/portfolio/code_icons/ruby.png'></div><div class='body'>Terminal chess implementation, written in Ruby. &nbsp;<a href='http://github.com/karenling/chess'><i class='fa fa-github'></i></a></div></div></div>");
+    //
+    //     }
+    //   } else {
+    //     $('.main').css('position', 'absolute');
+    //     $('.main').css('top', windowHeight * 3);
+    //   }
+    //
+    //   $('.down-arrow').click(function () {
+    //     $('html, body').stop();
+    //     var scrollPos = $('body').scrollTop();
+    //     var increment = windowHeight;
+    //
+    //     if ((scrollPos >= windowHeight * 2) && (scrollPos < windowHeight * 3)) {
+    //       increment = windowHeight * 54;
+    //     }
+    //     $('html, body').animate({
+    //       scrollTop: scrollPos += increment
+    //     }, 100);
+    //   })
+    //
+    //   $('.mail').click(function () {
+    //     $('html, body').stop();
+    //     $('html, body').animate({
+    //       scrollTop: $(window).height() * 4
+    //     }, 1000);
+    //   });
 
-    $(window).scroll(function () {
-      var currentPos = $('body').scrollTop();
-      if (currentPos < windowHeight * 3) { // IMPORTANT TO SET THIS TO THE NUMBER OF IMAGES THERE ARE
-
-        if (currentPos >= 0 && currentPos < windowHeight) {
-          $('.main').css('position', 'fixed');
-          $('.main').css('top', 0);
-          $('.portfolio').css('background-image', "url('http://images.karenling.net/aa/portfolio/portfolio_image_1.jpg')");
-          $('.portfolio').html("<div class='description'><div class='content'><div class='title'><a href='http://flappypig.karenling.net/'>FlappyPig <i class='fa fa-external-link'></i></a></div><div class='icons'><img src='https://dl.dropboxusercontent.com/u/2330299/capstone/portfolio/code_icons/javascript.png'><img src='https://dl.dropboxusercontent.com/u/2330299/capstone/portfolio/code_icons/html5.png'></div><div class='body'>Highly addictive video game, inspired by Flappy Bird, BUT with a flying pig! I built the game using JavaScript and HTML5 Canvas. &nbsp;<a href='http://github.com/karenling/Flappy-Pig'><i class='fa fa-github'></i></a></div></div></div>");
-        } else if (currentPos >= windowHeight && currentPos < windowHeight * 2) {
-          $('.main').css('position', 'fixed');
-          $('.main').css('top', 0);
-          $('.portfolio').css('background-image', "url('http://images.karenling.net/aa/portfolio/portfolio_image_2.jpg')");
-          $('.portfolio').html("<div class='description'><div class='content'><div class='title'><a href='http://twinkieset.com'>Twinkieset <i class='fa fa-external-link'></i></a></div><div class='icons'><img src='https://dl.dropboxusercontent.com/u/2330299/capstone/portfolio/code_icons/rails.png'><img src='https://dl.dropboxusercontent.com/u/2330299/capstone/portfolio/code_icons/ruby.png'><img src='https://dl.dropboxusercontent.com/u/2330299/capstone/portfolio/code_icons/javascript.png'><img src='https://dl.dropboxusercontent.com/u/2330299/capstone/portfolio/code_icons/backbonejs.png'><img src='https://dl.dropboxusercontent.com/u/2330299/capstone/portfolio/code_icons/html5.png'><img src='https://dl.dropboxusercontent.com/u/2330299/capstone/portfolio/code_icons/css3.png'></div><div class='body'>Twinkieset is a client photo gallery for modern photographers, inspired by <a href='http://pixieset.com'>Pixieset</a>, built on Rails and Backbone. Photographers can upload photos and create beautifully designed galleries for their clients. &nbsp;<a href='http://github.com/karenling/Twinkieset'><i class='fa fa-github'></i></a></div></div></div>");
-        } else if (currentPos >= windowHeight * 2 && currentPos < windowHeight * 3) {
-          $('.main').css('position', 'fixed');
-          $('.main').css('top', 0);
-          $('.portfolio').css('background-image', "url('http://images.karenling.net/aa/portfolio/portfolio_image_3.jpg')");
-          $('.portfolio').html("<div class='description'><div class='content'><div class='title'><a href='http://github.com/karenling/chess'>Ruby Chess <i class='fa fa-external-link'></i></a></div><div class='icons'><img src='https://dl.dropboxusercontent.com/u/2330299/capstone/portfolio/code_icons/ruby.png'></div><div class='body'>Terminal chess implementation, written in Ruby. &nbsp;<a href='http://github.com/karenling/chess'><i class='fa fa-github'></i></a></div></div></div>");
-
-        }
-      } else {
-        $('.main').css('position', 'absolute');
-        $('.main').css('top', windowHeight * 3);
-      }
-      //
-      // $('.down-arrow').click(function () {
-      //   $('html, body').stop();
-      //   var scrollPos = $('body').scrollTop();
-      //   var increment = windowHeight;
-      //
-      //   if ((scrollPos >= windowHeight * 2) && (scrollPos < windowHeight * 3)) {
-      //     increment = windowHeight * 54;
-      //   }
-      //   $('html, body').animate({
-      //     scrollTop: scrollPos += increment
-      //   }, 100);
-      // })
-      //
-      // $('.mail').click(function () {
-      //   $('html, body').stop();
-      //   $('html, body').animate({
-      //     scrollTop: $(window).height() * 4
-      //   }, 1000);
-      // });
-
-    });
+    // });
 
     $(window).scroll();
 
