7834d247dee7f941d2405481063d60db3ef26f3f
Karen Ling <karensling@gmail.com>
2015-07-01 00:42:53 -0700

    Remove flashes.

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,10 +4,8 @@ class MessagesController < ApplicationController
     @message = Message.new(message_params)
 
     if @message.save
-      flash[:notice] = ["Sent!"]
       render json: @message, status: :ok
     else
-      flash[:errors] = ["Errors"]
       render json: @message.errors.full_messages, status: :unprocessable_entity
     end
   end
