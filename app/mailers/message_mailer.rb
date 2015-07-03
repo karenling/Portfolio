3d67b99ca6eea808a94e7003a53a0e912711001c
Karen Ling <karensling@gmail.com>
2015-07-03 09:59:26 -0700

    Include html safe.

--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -2,9 +2,9 @@ class MessageMailer < ApplicationMailer
   default from: "admin@twinkeiset.com"
 
   def notification_email(name, email, body)
-    @name = name
-    @email = email
-    @body = body
+    @name = name.html_safe
+    @email = email.html_safe
+    @body = body.html_safe
     mail(to: 'karensling@gmail.com', subject: 'message from twinkieset.com')
   end
 end
