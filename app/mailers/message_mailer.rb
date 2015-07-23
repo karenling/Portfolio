9a12fcca368266187c1d6f2e468d0989cc9e7f44
Karen Ling <karensling@gmail.com>
2015-07-23 00:48:24 -0700

    Change twinkieset email to karenling.

--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -1,10 +1,10 @@
 class MessageMailer < ApplicationMailer
-  default from: "admin@twinkeiset.com"
+  default from: "admin@karenling.net"
 
   def notification_email(name, email, body)
     @name = name.html_safe
     @email = email.html_safe
     @body = body.html_safe
-    mail(to: 'karensling@gmail.com', subject: 'message from twinkieset.com')
+    mail(to: 'karensling@gmail.com', subject: 'message from karenling.net')
   end
 end
